73af5df396d217997c49e4c3dfd39de1589fb053
Kirara02 <fathul.sirnaraja92@gmail.com>
2022-09-19 15:09:50 +0700

    secsion kedua part ketiga

--- resources/js/Pages/Prototype/Dashboard.js
+++ resources/js/Pages/Prototype/Dashboard.js
@@ -1,5 +1,62 @@
 import Authenticated from "@/Layouts/Authenticated/Index";
+import Flickity from "react-flickity-component";
+import { Head } from "@inertiajs/inertia-react";
+import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
 
+const flickityOptions = {
+    cellAlign: "left",
+    contain: true,
+    groupCells: 1,
+    wrapAround: false,
+    pageDots: false,
+    prevNextButtons: false,
+    draggable: ">1",
+};
 export default function Dashboard() {
-    return <Authenticated />;
+    return (
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+                <title>Dashboard</title>
+            </Head>
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {/* Movie Thumbnail */}
+                    {[1, 2, 3, 4].map((i) => (
+                        <FeaturedMovie
+                            key={i}
+                            slug="the-batman-in-love"
+                            name={`Batman In Love ${i}`}
+                            category="Action"
+                            thumbnail="/images/featured-1.png"
+                            rating={i + 1}
+                        />
+                    ))}
+                </Flickity>
+            </div>
+            <div className="mt-[50px]">
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Browse
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {[1, 2, 3, 4, 6].map((i) => (
+                        <MovieCard
+                            key={i}
+                            slug="the-batman-in-love"
+                            name={`Batman In Love ${i}`}
+                            category="Action"
+                            thumbnail="/images/featured-1.png"
+                        />
+                    ))}
+                </Flickity>
+            </div>
+        </Authenticated>
+    );
 }
